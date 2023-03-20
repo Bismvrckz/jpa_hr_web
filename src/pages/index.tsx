e8da628850a20a1a8ad2b194d537548544bf7d33
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2023-03-20 10:41:11 +0700

    sequelize init

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,16 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  return <div>hello init</div>;
+  return (
+    <div className="w-[100vw] h-[100vh] text-[3rem] flex items-center justify-center">
+      <Link href="/jobcat">
+        <p>hello init</p>
+      </Link>
+    </div>
+  );
 }
---
+++ src/pages/jobcat.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function jobcat() {
+  return <div>jobcat</div>;
+}
+
+export default jobcat;
