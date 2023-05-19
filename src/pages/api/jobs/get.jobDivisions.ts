bc9d376816054908b8570e1d5e53e9da5f51766e
Ahmad  Syamil <ahmadfayruzsyamil33@gmail.com>
2023-05-19 12:49:45 +0700

    fix endpoint

--- src/pages/api/jobs/get.jobDivisions.ts
+++ src/pages/api/jobs/get.jobDivisions.ts
@@ -5,36 +5,48 @@ const { job_divisions } = require("../../../../models");
 
 const handler = nextConnect();
 
-handler.get(async (req: NextApiRequest, res: NextApiResponse) => {
-  try {
-    const { job_divisions_id, limit, offset, avoid } = req.query;
+interface RequestQuery {
+  job_divisions_id: string;
+  limit: string;
+  offset: string;
+  avoid: string;
+}
 
-    if (job_divisions_id == "all" || !job_divisions_id) {
-      const resGetJobDivisions = await job_divisions.findAll({
-        where: {
-          // postingStatus: {
-          //   [Op.not]: "ARCHIVE",
-          // },
-          job_divisions_id: {
-            [Op.not]: avoid || null,
+handler.get(
+  async (
+    req: NextApiRequest & { query: RequestQuery },
+    res: NextApiResponse
+  ) => {
+    try {
+      const { job_divisions_id, limit, offset, avoid } = req.query;
+
+      if (job_divisions_id == "all" || !job_divisions_id) {
+        const resGetJobDivisions = await job_divisions.findAll({
+          where: {
+            // postingStatus: {
+            //   [Op.not]: "ARCHIVE",
+            // },
+            job_divisions_id: {
+              [Op.not]: avoid || null,
+            },
           },
-        },
-        limit: parseInt(limit) || null,
-        offset: offset || null,
+          limit: parseInt(limit) || null,
+          offset: offset || null,
+        });
+
+        return res.send({ status: "Success", resGetJobDivisions });
+      }
+
+      const resGetJobDivisions = await job_divisions.findOne({
+        where: { job_divisions_id },
       });
 
-      return res.send({ status: "Success", resGetJobDivisions });
+      res.send({ status: "Success", resGetJobDivisions });
+    } catch (error) {
+      console.log({ error });
+      res.send({ error });
     }
-
-    const resGetJobDivisions = await job_divisions.findOne({
-      where: { job_divisions_id },
-    });
-
-    res.send({ status: "Success", resGetJobDivisions });
-  } catch (error) {
-    console.log({ error });
-    res.send({ error });
   }
-});
+);
 
 export default handler;
